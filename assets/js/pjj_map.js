217f745896b5b3bbe3a29aa3d00fc9b791bd43db
Aaron Oxborrow <aaron@pastedesign.com>
2011-07-06 01:34:22 -0600

    added test locations and skeleton for  map projection

--- assets/js/pjj_map.js
+++ assets/js/pjj_map.js
@@ -1,3 +1,5 @@
+// Photojojo SVG Status Map
+
 // get query string params
 // http://stackoverflow.com/questions/901115/get-query-string-values-in-javascript/2880929#2880929
 var urlParams = {};
@@ -12,38 +14,122 @@ var urlParams = {};
        urlParams[d(e[1])] = d(e[2]);
 })();
 
-// setup DEBUG flag for development
-var DEBUG = ("debug" in urlParams);
+// takes a lat/long pair and draws a red dot on the map, with a label for debugging
+function pathTest(testNum, lat, long, label) {
 
-// change map background to real map for debug
-if (DEBUG) {
-	$('#paper').css("background-image", "url('./assets/images/realmap_bg.jpg')"); 
+	// origin coordinates (Mobridge, SD)
+	var originX = 431;
+	var originY = 116;
+
+	// compute coordinates
+	var destX = projectLat(lat);
+	var destY = projectLong(long);
+
+	// add a label
+	if (label !== undefined) {
+		// add white background for legibility
+		var lbl_bg = map.text(destX+5, destY, label).attr({'font-family': 'sans-serif', 'font-weight': 'bold', 'font-size': 12, fill: '#fff', 'text-anchor': 'start', stroke: '#fff', 'stroke-width': 4, 'stroke-linecap': 'round', 'stroke-linejoin': 'round'})
+		var lbl = map.text(destX+5, destY, label).attr({'font-family': 'sans-serif', 'font-weight': 'bold', 'font-size': 12, fill: '#f00', 'text-anchor': 'start'})
+	}
+
+	// draw red map marker
+	var mrkr = map.ellipse(destX, destY, 2, 2).attr({stroke: "none", fill: "#f00"}).translate(-1, -1);	
+	
+	// don't draw a path for origin
+	if (originX == destX && originY == destY) {
+		return;
+	}
+	
+	setTimeout(function() {
+	
+		// midpoint coordinates (for curve)
+		var midpointX = (originX+destX) / 2;
+		var midpointY = (originY+destY) / 2;
+
+		// draw shipping path from origin
+		var pathSVG = 'M '+originX+' '+originY+' Q '+(midpointX-30)+' '+(midpointY+80)+' '+destX+' '+destY;
+		
+		// draw shipping path
+		var ship_path = map.path(pathSVG).attr({opacity: .5, stroke: '#222', 'stroke-width': 2, 'stroke-linecap': 'round', 'stroke-linejoin': 'round', 'stroke-dasharray': '- '});
+		
+		// package marker
+		var marker = map.ellipse(originX, originY, 6, 6).attr({stroke: "none", fill: "#66cccc"});
+		marker.animateAlong(ship_path, 1000, function() {
+			this.hide();
+		});
+	
+	}, (testNum-1)*1000);
+	
+	
 }
+
+// project latitude onto map, return X coordinate
+function projectLat(lat) {
+
+	// TODO: map projection
+	return lat;
+	
+}
+
+// project longitude onto map, return Y coordinate
+function projectLong(long) {
+	
+	// TODO: map projection
+	return long;
+	
+}
+
+// setup DEBUG flag for development
+var DEBUG = ('debug' in urlParams);
 
 // setup raphael object
-var paper = Raphael(document.getElementById('paper'), 970, 500);
+var map = Raphael(document.getElementById('map'), 970, 500);
 
 // draw usa outline
-var usa = paper.path(pjjMapAssets.usa).attr({opacity: '.8', stroke: '#92724d', 'stroke-width': 3, 'stroke-linejoin': 'round', 'stroke-linecap': 'round'});
+var usa = map.path(mapAssets.usa).attr({opacity: '.8', stroke: '#92724d', 'stroke-width': 3, 'stroke-linejoin': 'round', 'stroke-linecap': 'round'});
 
-// draw customer icon
-var customer = paper.set();
-paper.importSVG(pjjMapAssets.customerSVG, customer);
-customer.scale(.7, .7).translate(120, 85);
+if (DEBUG) {
 
-// draw warehouse icon
-var warehouse = paper.path(pjjMapAssets.warehouse).attr({fill: '#222', stroke: '#222', 'stroke-width': .5, 'stroke-linejoin': 'round', 'stroke-linecap': 'round'}).translate(625, 200);
+	// change map background to real map for debug
+	$('#map').css('background-image', "url('./assets/images/realmap_bg.jpg')");
+	
+} else {
+		
+	// draw customer icon
+	var customer = map.set();
+	map.importSVG(mapAssets.customerSVG, customer);
+	customer.scale(.7, .7).translate(120, 85);
 
-// draw PJJ HQ
-if (! DEBUG) {
-	var hq = paper.image("./assets/images/pjj_hq.png", 80, 243, 80, 60);
+	// draw warehouse icon
+	var warehouse = map.path(mapAssets.warehouse).attr({fill: '#222', stroke: '#222', 'stroke-width': .5, 'stroke-linejoin': 'round', 'stroke-linecap': 'round'}).translate(625, 200);
+
+	// draw PJJ HQ
+	var hq = map.image("./assets/images/pjj_hq.png", 80, 243, 80, 60);
+
+	// draw example shipping path
+	var pathSVG = 'M 660 245 c0,150 -200-100 -200,0 s-100,50 -100,0 s50,-50 50,0 s-100,50 -100,0 s50,-100 -130,-100';
+	var ship_path = map.path(pathSVG).attr({stroke: '#3b4449', 'stroke-width': 3, 'stroke-linecap': 'round', 'stroke-linejoin': 'round', 'stroke-dasharray': '- '}); 
+
+	// yellow location marker
+	var marker = map.ellipse(660, 245, 8, 8).attr({stroke: "none", fill: "#ff0"});
+	marker.animateAlong(ship_path, 8000, function() {
+		this.hide();
+	});
+	
 }
 
-// draw shipping path
-var ship_path = paper.path('M 660 245 c0,150 -200-100 -200,0 s-100,50 -100,0 s50,-50 50,0 s-100,50 -100,0 s50,-100 -130,-100').attr({stroke: '#3b4449', 'stroke-width': 3, 'stroke-linecap': 'round', 'stroke-linejoin': 'round', 'stroke-dasharray': '- '}); 
+// setup some locations for projection testing
+var tests = [];
+tests[0] = [431, 116, 'Mobridge'];
+tests[1] = [123, 74, 'Seattle'];
+tests[2] = [271, 210, 'Salt Lake City'];
+tests[3] = [122, 263];
+tests[4] = [268, 338, 'Phoenix'];
+tests[5] = [470, 392, 'Austin'];
+tests[6] = [700, 419, 'Orlando'];
+tests[7] = [696, 195, 'Cleveland'];
 
-// yellow location marker
-var marker = paper.ellipse(664, 251, 8, 8).attr({stroke: "none", fill: "#ff0"}).translate(-4, -4);
-marker.animateAlong(ship_path, 8000, function() {
-	this.hide();
-});
+// test some locations
+for (var l = 0; l < tests.length; l++) {
+	pathTest(l, tests[l][0], tests[l][1], tests[l][2]);	
+}
